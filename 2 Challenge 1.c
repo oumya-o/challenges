1671d54a85201e0ef1fccb9422613305a040fc9b
oumya-o <oussamaoumya11@gmail.com>
2024-09-25 11:33:18 +0200

    Conditions

---
+++ 2 Challenge 1.c
@@ -0,0 +1,13 @@
+int main() {
+    
+    int A ;
+    printf("Entre un nbr  :");
+    scanf("%d", &A);
+    if( A%2==0 ){
+        printf("pair");
+    }else {
+    printf("impair");
+    }
+
+    return 0;
+}
