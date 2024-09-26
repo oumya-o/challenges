88a7dd4ba60f6c2ab7cdfce8be377460ee90dd46
oumya-o <oussamaoumya11@gmail.com>
2024-09-26 14:59:00 +0200

    Loop 2 edited , Loop 3 done

--- les Boucles 3.c
+++ les Boucles 3.c
@@ -1,16 +1,15 @@
 #include <stdio.h>
 
 int main() {
-  int  N , f=1 ,i ;
+int  N , f=0 ,i ;
     printf ("Entre une Number : ");
     scanf ("%d", &N);
      for (i=1 ; i<=N ; i++) {
          
-    f*=i;
+    f+=i;
      }
     printf("%d" ,f );
    
-
-    
-    return 0;
+   
+  return 0;
 }
