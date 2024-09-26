91010d778d95d46bb820f33617148395d87324e1
oumya-o <oussamaoumya11@gmail.com>
2024-09-26 16:24:27 +0200

    Les boucles 4&6

---
+++ Les boucles 6.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+int  N , i ;
+    printf ("Entre une Number : ");
+    scanf ("%d", &N);
+    for (i=0; i < N ; i++ ){
+    printf("%d\n" ,2 * i );
+    }
+  return 0;
+}
