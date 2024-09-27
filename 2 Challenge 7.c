31a10efdfb7e2916580e36a9b70354f81e77ab79
oumya-o <oussamaoumya11@gmail.com>
2024-09-27 10:16:48 +0200

    Conditions Chl 7 edited

--- 2 Challenge 7.c
+++ 2 Challenge 7.c
@@ -5,7 +5,7 @@ int main() {
    printf("Entre la letter : ");
    scanf("%c", &L);
    
-   if ( 'A' <= L && L <= 'Z'){
+   if ( 65 <= L && L <= 90){
        printf("La letter est Majuscule");
    }else{
        printf("la lettre est Miniscule");
