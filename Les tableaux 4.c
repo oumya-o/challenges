171acc1e111fd59b851347ce31ab43c7b26dcdda
oumya-o <oussamaoumya11@gmail.com>
2024-09-28 16:50:19 +0200

    Les tableaux 4

---
+++ Les tableaux 4.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+    int n, i;
+    int M;
+
+    printf(" Combien de numbers ");
+    scanf("%d", &n);
+
+    int tableau[100]; 
+
+
+    printf("Entre les number :\n");
+    for (i = 0; i < n; i++) {
+        scanf("%d", &tableau[i]);
+    }
+
+    M = tableau[0];
+
+    for (i = 1; i < n; i++) {
+        if (tableau[i] > M) {
+            M = tableau[i];
+        }
+    }
+
+    printf("le grand number entre les %d numbers est : %d\n", n, M);
+
+    return 0;
+}
