b6ea44329166190944818939bfc5ae8f1065420e
oumya-o <oussamaoumya11@gmail.com>
2024-09-30 13:49:44 +0100

    Tableaux 3 modified

--- Les tableaux 3.c
+++ Les tableaux 3.c
@@ -1,15 +1,22 @@
 #include <stdio.h>
+
 int main() {
-    int n, i, sum = 0;
-        printf("Entre la taille : ");
-        scanf("%d", &n);
-    int T[100]; 
-        printf("Entre les éléments :\n");
-    for (i = 0; i < n; i++) {
-       
-        scanf("%d", &T[i]);
-        sum += T[i];
+int   n , S;
+printf (" entre la taille :");
+scanf("%d",&n);
+int T[n];
+printf("Entre les éléments :\n");
+  for (int i = 0 ; i < n; i++){
+    printf ("T[%d]=",i);
+      scanf ("%d",&T[i]);
+  }  
+  printf("Les element sont:\n");
+for (int i = 0; i < n; i++) {
+        
+        printf("T[%d] = %d\n", i,T[i]);
+        S=S+T[i];
+      
     }
-    printf("La sum de number est : %d\n", sum);
+  printf("La somme est :%d", S);
     return 0;
 }
