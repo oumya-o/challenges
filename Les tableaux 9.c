0e0651713b9de223e0ea15ebf71b2867925a0c0b
oumya-o <oussamaoumya11@gmail.com>
2024-09-30 14:48:19 +0100

    Les tableaux 9

---
+++ Les tableaux 9.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+int   n , Multi , F;
+printf ("Entre la taille de tableau :");
+scanf("%d",&n);
+int T[n];
+printf("Entre les éléments :\n");
+  for (int i = 0 ; i < n; i++){
+    printf ("T[%d]=",i);
+      scanf ("%d",&T[i]);
+  }
+  printf (" Les number on onrdre Croissant\n");
+  for (int i = n-1 ; i >= 0; i--){
+    
+      printf("T[%d] = %d\n" , i, T[i]);
+      
+  }
+  
+  
+    return 0;
+}
