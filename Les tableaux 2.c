ec8713f2630ba2efada926c15347e3c8de940a0d
oumya-o <oussamaoumya11@gmail.com>
2024-09-30 12:00:32 +0100

    Modified Tables 2

--- Les tableaux 2.c
+++ Les tableaux 2.c
@@ -1,24 +1,17 @@
-
-
 #include <stdio.h>
 
 int main() {
-    int n;
-
-    printf("Entre la taille :");
-    scanf("%d", &n);
-
-    int T[n];
-
-    printf("Entre les éléments :\n");
-    
-    for (int i = 0; i < n; i++) {
-        
-        scanf("%d", &T[i]);
-    }
-
-    printf("Alors :\n");
-    for (int i = 0; i < n; i++) {
+int   n ;
+printf (" entre la taille :");
+scanf("%d",&n);
+int T[n];
+printf("Entre les éléments :\n");
+  for (int i = 0 ; i < n; i++){
+    printf ("T[%d]=",i);
+      scanf ("%d",&T[i]);
+  }  
+  printf("Les element sont:\n");
+for (int i = 0; i < n; i++) {
         
         printf("T[%d] = %d\n", i,T[i]);
     }
