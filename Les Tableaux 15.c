cd7a624dd9ea9fcd77fb5dab15107623c200cdb4
oumya-o <oussamaoumya11@gmail.com>
2024-10-01 10:24:19 +0100

    EXE 15 les tableaux

---
+++ Les Tableaux 15.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main() {
+   int n , x;
+       printf ("Entre la taille de la 1er Tab :");
+       scanf ("%d",&n);
+       printf ("Entre la taille de la 2eme Tab :");
+       scanf ("%d",&x);
+   int T1[n] , T2[x];
+       printf("Entrez les element de la 1er tab\n");
+     for (int i=0 ;i<n; i++){
+       printf("T1[%d]=",i);
+       scanf("%d",&T1[i]);
+       }
+       printf("Entrez les element de la 2eme tab\n");
+     for (int i=0 ;i<x; i++){
+       printf("T2[%d]=",i);
+       scanf("%d",&T2[i]);
+     }
+     int T3[x+n];
+       printf("les element de la 3eme tab sont :\n");
+  for (int i = 0; i < n; i++) {
+        T3[i] = T1[i];
+    }
+    for (int i = 0; i < x; i++) {
+        T3[n + i] = T2[i];
+    }
+
+    printf("Les elements de la 3eme tab sont :\n");
+    for (int i = 0; i < x + n; i++) {
+        printf("T3[%d]=%d\n", i, T3[i]);
+    }
+
+    return 0;
+}
